f49dd5e4db870c7bd3073fa21c034c23a8197812
arahosu <arahosu@gmail.com>
2020-02-08 23:07:29 +0000

    Background Substraction draft

---
+++ src/Background_substraction.py
@@ -0,0 +1,51 @@
+import cv2 
+import numpy as np
+from PIL import Image
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+frame_counter = 0
+frames = []
+
+while ret==True and frame_counter <5:
+    ret, frame = cap.read()
+    frame_counter += 1
+    cv2.imshow('b', frame)
+    frames.append(frame)
+    k = cv2.waitKey(30) & 0xFF
+    if k==27:
+        break
+
+avg_img = np.zeros(shape=[480, 640, 3], dtype=np.uint8)
+
+for i in range(len(frames)):
+    avg_img = avg_img + frames[2]
+
+#avg_img = avg_img/(len(frames)-1)
+
+cv2.imwrite('C:\\Users\\Joonsu\\source\\repos\\CiscoBots\\avg_img.jpg', avg_img )
+cap.release()
+cv2.destroyAllWindows()
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+
+cv2.imshow('0', frame[:,:,0])
+cv2.imshow('1', frame[:,:,1])
+cv2.imshow('2', frame[:,:,2])
+cv2.imshow(' ', frame)
+
+while ret== True:
+    ret, frame = cap.read()
+    img_sub = frame - avg_img
+    obj_1, th1 = cv2.threshold(img_sub[:,:,1]*2, 150, 255, cv2.THRESH_BINARY)
+    obj_2, th2 = cv2.threshold(img_sub[:,:,2]*2, 150, 255, cv2.THRESH_BINARY)
+  
+    cv2.imshow('c', th1)
+    cv2.imshow('f', th2)
+    k = cv2.waitKey(30) & 0xFF1
+    if k==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
