adbd54d61ea21351417fbe9f9d9e3887f6b7fddd
Joearrowsmith <joearrowsmith98@gmail.com>
2020-02-08 20:52:29 +0000

    adding dancing bot code

--- public/camera.py
+++ public/camera.py
@@ -2,7 +2,7 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture()
+        self.video = cv2.VideoCapture(0)
 
     def __del__(self):
         self.video.release()        
