f696d85e6f64363952ec67875ee3eadc212c681a
pv09 <44298046+pv09@users.noreply.github.com>
2020-02-09 03:52:53 +0000

    found boundary pixels

--- GAME/battlefield.py
+++ GAME/battlefield.py
@@ -14,7 +14,6 @@ class Arena:
             self.boundary_corners[self.corner_pointer] = (x, y)
             if (self.corner_pointer == 0):
                 self.first_point = (x, y)
-                print(self.first_point)
             self.corner_pointer = self.corner_pointer + 1
 
             if (self.corner_pointer%2 == 0):
@@ -32,11 +31,10 @@ class Arena:
 
         
     def draw_lines(self, corners1, corners2):
-        cv2.line(self.image, corners1, corners2, (255, 0, 0), 3)
+        cv2.line(self.image, corners1, corners2, (255, 0, 0), 2)
         cv2.imshow('try', framein)
 
     def border_set_done(self):
-        print('in')
         self.set_boundary = False
 
     def show(self):
@@ -52,18 +50,26 @@ field_w = 500 #height and wifth of the battlefield
 field_h = 500
 
 framein = np.zeros((field_w, field_h, 3), dtype="uint8")
+anatra = np.zeros((field_w, field_h, 3), dtype="uint8")
 cv2.imshow('try', framein)
 
 arena = Arena(framein)
 arena.set_borders()
 
 #Get the pixels of the border
+print(framein[arena.first_point[1], arena.first_point[0], :])
+
+cv2.line(anatra, (100, 100), (100, 103), (255, 0, 0), 2)
+
+
 print("out of the loop")
-border_px = np.where(framein == (255, 0, 0))
-print(len(border_px))
-print(border_px[0])
-print(border_px[0])
-print(border_px[0])
+border_px = np.where(np.all(anatra == (255, 0, 0), -1))
+print(anatra[103, 101, :])
+# border_px = np.reshape(border_px, (len(border_px[0]),3))
+# print(len(border_px))
+print(border_px)
+# print(border_px[0])
+# print(border_px[0])
 
 
 
