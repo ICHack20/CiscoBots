1bd6516d52d5aeb5b0a2115a7ed458e4357c7cab
Joearrowsmith <joearrowsmith98@gmail.com>
2020-02-09 04:14:30 +0000

    adding bots server and control

---
+++ bots/bots_server.py
@@ -0,0 +1,52 @@
+## needs to be added to start up
+
+import asyncio
+import websockets
+
+actions = {
+    "forward": (1, 1),
+    "backward": (-1, -1),
+    "right": (0, 1),
+    "left": (1, 0),
+}
+
+from gpiozero import Robot
+
+left_pins = (24, 25)
+right_pins = (18, 23)
+
+robot = Robot(left_pins, right_pins)
+
+run_robot = True
+
+async def control(websocket, path):
+    action = await websocket.recv()
+    print(f"< {action}")
+    
+    valid_move = False
+    
+
+    if action == "forward":
+        print("going forward")
+        valid_move = True
+    elif action == "backward":
+        print("going backward")
+        valid_move = True
+    elif action == "right":
+        print("going right")
+        valid_move = True
+    elif action == "left":
+        print("going left")
+        valid_move = True
+    
+    if valid_move and run_robot:
+        robot.value = actions[action]
+
+    resp = f"{action}-{msg}"
+    await websocket.send(resp)
+    print(f"> {resp}")
+
+start_server = websockets.serve(control, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ bots/control_client.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+import asyncio
+import websockets
+
+async def send_action(action, uri="ws://localhost:8765"):
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(action)
+        resp = await websocket.recv()
+
+def key_press(event):
+    if event.key == "up":
+        print("send up")
+        asyncio.get_event_loop().run_until_complete(send_action("forward"))
+    elif event.key == "down":
+        print("send down")
+        asyncio.get_event_loop().run_until_complete(send_action("backward"))
+    elif event.key == "right":
+        print("send right")
+        asyncio.get_event_loop().run_until_complete(send_action("right"))
+    elif event.key == "left":
+        print("send left")
+        asyncio.get_event_loop().run_until_complete(send_action("left"))
+
+def main():
+    fig = plt.figure()
+    fig.canvas.mpl_connect("key_press_event", key_press)
+    plt.show()
+
+if __name__=="__main__":
+    main()
